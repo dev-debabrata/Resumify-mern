c4c16ac8450199d630bbf689474a84c914e37d04
Debabrata Das <debabratadas711@gmail.com>
2025-12-27 21:44:12 +0530

    db connect

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,78 @@
-require("dotenv").config();
-const express = require('express');
-const cors = require("cors");
-const path = require("path");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
 
 
-const app = express()
+dotenv.config();
 
-
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
 
 
 
-// Start Server
-const PORT = process.env.PORT || 3000;
+// Middleware
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// ---------------- PRODUCTION FRONTEND SERVE ----------------
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.join(__dirname, "frontend", "dist", "index.html")
+        );
+    });
+}
+
+// Start Server
+app.listen(PORT, () => {
+    console.log("Server running on port: " + PORT)
+    connectDB()
+});
+
+
+
+
+// require("dotenv").config();
+// const express = require('express');
+// const cors = require("cors");
+// const path = require("path");
+// const { connectDB } = require("./lib/db");
+
+
+// const app = express()
+
+
+// app.use(
+//     cors({
+//         origin: process.env.CLIENT_URL || "*",
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         allowedHeaders: ["Content-Type", "Authorization"],
+//     })
+// );
+
+// // Middleware
+// app.use(express.json());
+
+
+
+// // Start Server
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+//     connectDB()
+// })
