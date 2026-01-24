9f5cdfbd39e6022edab1d91bda226280c6120a88
Debabrata Das <debabratadas711@gmail.com>
2026-01-24 23:36:23 +0530

    24-01-2026

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -12,6 +12,7 @@ const generateToken = (userId) => {
     );
 };
 
+
 export const registerUser = async (req, res) => {
     try {
         const { name, email, password, profileImageUrl } = req.body;
@@ -61,46 +62,6 @@ export const registerUser = async (req, res) => {
 };
 
 
-// export const registerUser = async (req, res) => {
-//     try {
-//         const { name, email, password, profileImageUrl } = req.body;
-
-//         // Check if user already exists
-//         const userExists = await User.findOne({ email });
-
-//         if (userExists) {
-//             return res.status(400).json({
-//                 message: "User already exists",
-//             });
-//         }
-
-//         // Hash password
-//         const salt = await bcrypt.genSalt(10);
-//         const hashedPassword = await bcrypt.hash(password, salt);
-
-//         // Create user
-//         const user = await User.create({
-//             name,
-//             email,
-//             password: hashedPassword,
-//             profileImageUrl,
-//         });
-
-//         // Return user data + token
-//         res.status(201).json({
-//             _id: user._id,
-//             name: user.name,
-//             email: user.email,
-//             profileImageUrl: user.profileImageUrl,
-//             token: generateToken(user._id),
-//         });
-//     } catch (error) {
-//         res.status(500).json({
-//             message: "Server error",
-//             error: error.message,
-//         });
-//     }
-// };
 
 export const loginUser = async (req, res) => {
     try {
@@ -161,6 +122,49 @@ export const getUserProfile = async (req, res) => {
         });
     }
 };
+
+
+// export const registerUser = async (req, res) => {
+//     try {
+//         const { name, email, password, profileImageUrl } = req.body;
+
+//         // Check if user already exists
+//         const userExists = await User.findOne({ email });
+
+//         if (userExists) {
+//             return res.status(400).json({
+//                 message: "User already exists",
+//             });
+//         }
+
+//         // Hash password
+//         const salt = await bcrypt.genSalt(10);
+//         const hashedPassword = await bcrypt.hash(password, salt);
+
+//         // Create user
+//         const user = await User.create({
+//             name,
+//             email,
+//             password: hashedPassword,
+//             profileImageUrl,
+//         });
+
+//         // Return user data + token
+//         res.status(201).json({
+//             _id: user._id,
+//             name: user.name,
+//             email: user.email,
+//             profileImageUrl: user.profileImageUrl,
+//             token: generateToken(user._id),
+//         });
+//     } catch (error) {
+//         res.status(500).json({
+//             message: "Server error",
+//             error: error.message,
+//         });
+//     }
+// };
+
 
 
 // export const loginUser = async (req, res) => {
