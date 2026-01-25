c409e9d02fde3f6b8f02a5026c3a02984191d5c3
Debabrata Das <debabratadas711@gmail.com>
2026-01-25 22:18:09 +0530

    dashboard add

--- frontend/src/components/layout/DashboardLayout.jsx
+++ frontend/src/components/layout/DashboardLayout.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { Children, useContext } from 'react'
+import { UserContext } from '../../context/UserContext';
+import Navbar from './Navbar';
 
 const DashboardLayout = () => {
+    const { user } = useContext(UserContext);
     return (
-        <div>DashboardLayout</div>
+        <div>
+            <Navbar activeManu={activeManu} />
+
+            {user && <div className="container mx-auto pt-4 pb-4">{Children}</div>}
+        </div>
     )
 }
 
--- frontend/src/pages/Home/Dashboard.jsx
+++ frontend/src/pages/Home/Dashboard.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Dashboard = () => {
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
-}
+    const navigate = useNavigate();
 
-export default Dashboard
+    const [openCreateModal, setOpenCreateModal] = useState(false);
+    const [allResumes, setAllResumes] = useState(null);
+
+    const fetchAllResumes = async () => {
+        try {
+            const response = await axiosInstance.get(API_PATHS.RESUME.GET_ALL);
+            setAllResumes(response.data);
+        } catch (error) {
+            console.error("Error fetching resumes:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchAllResumes();
+    }, []);
+
+    return <div>Dashboard</div>;
+};
+
+export default Dashboard;
